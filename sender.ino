d8422bef49150445b4216e4caec87a7783bc2026
citycafe578 <chentim102@gmail.com>
2025-02-11 22:29:28 +0800

    Update sender.ino

--- sender.ino
+++ sender.ino
@@ -3,47 +3,13 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-<<<<<<< HEAD
 RF24 radio(4, 5);
 const byte address[6] = "00001";
-int modes = 1;
+int modes = 0;
 // TaskHandle_t clockModeHandle = NULL;
 
 void clock_mode(void *pvParam) {
   for (;;) {
-=======
-RF24 radio(17, 5);
-const byte address[][6] = {"00001", "00002"};
-int data[4] = {};
-bool mode = 0; // 0 == TX, 1 == RX
-bool connected = false;
-int lost_connected = 0;
-int send_data = 1232;
-int request_data = 0001;
-
-void clock(void *pvParam){
-  vTaskDelay(200 / portTICK_PERIOD_MS);
-  mode++;
-  if(mode > 2){
-    mode = 0;
-  }
-}
-
-void nrf_receive_Task(void *pvParam){
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  while (1) {
-    if(mode == 1){
-
-    }
-    radio.startListening();
-    if (radio.available()){
-      read_data();
-    }
-    for(int i = 0; i < 4; i++){
-      Serial.print(data[i]);
-    }
-    Serial.println();
->>>>>>> 5d68cb1f85fab6038cca8ad08e61beab028b80de
     vTaskDelay(200 / portTICK_PERIOD_MS);
     modes = (modes + 1) % 2;
   }
@@ -64,12 +30,11 @@ void setup() {
   xTaskCreatePinnedToCore(clock_mode, "clock_mode", 2048, NULL, 1, NULL, 1);
 }
 
-<<<<<<< HEAD
 void loop() {
   if(modes == 0){
     radio.stopListening();
     delay(10);
-    const char text[] = "Hello, ground";
+    const char text[] = "Hello, drone";
     radio.write(&text, sizeof(text));
     Serial.println("message sent!");
   }else{
@@ -80,17 +45,8 @@ void loop() {
       radio.read(&text, sizeof(text));
       Serial.print("Received: ");
       Serial.println(text);
-=======
-
-
-void read_data(){
-  char receivedData[64] = {0};
-  radio.read(&receivedData, sizeof(receivedData));
-  char *token = strtok(receivedData, " ");
-  int index = 0;
-  while (token != NULL && index < 4) {
-    data[index] = atoi(token);
-    index++;
-    token = strtok(NULL, " ");
+    }else{
+      Serial.println("can't receive the message");
+    }
   }
 }
