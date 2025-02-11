9a112ba8a7066b015e05599505a985befe08d2f0
citycafe578 <chentim102@gmail.com>
2025-02-11 22:27:09 +0800

    back to begining
    ㄝ怎麼說
    我原本搞了個先握手再傳輸
    但是握手第二次永遠用不好
    而且非常不穩定
    所以先回到沒有握手的版本
    很暴力但是很穩定

--- receiver.ino
+++ receiver.ino
@@ -1,81 +1,52 @@
+#include <SPI.h>
 #include <RF24.h>
-#include <Wire.h>
-#define SDA 21
-#define SCL 22
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-RF24 radio(17, 5);
-const byte address[][6] = {"00001", "00002"};
-int data[4] = {};
-bool mode = 1; // 0 == TX, 1 == RX
-int send_data = 00000;
+RF24 radio(4, 5);
+const byte address[6] = "00001";
+int modes = 0;
+// TaskHandle_t clockModeHandle = NULL;
 
-void clock(void *pvParam){
-  vTaskDelay(200 / portTICK_PERIOD_MS);
-  mode++;
-  if(mode > 2){
-    mode = 0;
-  }
-}
-
-void nrf_receive_Task(void *pvParam){
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  while (1) {
-    if(mode == 1){
-
-    }
-    radio.startListening();
-    if (radio.available()){
-      char receivedData[64] = {0};
-      radio.read(&receivedData, sizeof(receivedData));
-      char *token = strtok(receivedData, " ");
-      int index = 0;
-      while (token != NULL && index < 4) {
-        data[index] = atoi(token);
-        index++;
-        token = strtok(NULL, " ");
-      }
-    }
-    for(int i = 0; i < 4; i++){
-      Serial.print(data[i]);
-    }
-    Serial.println();
+void clock_mode(void *pvParam) {
+  for (;;) {
     vTaskDelay(200 / portTICK_PERIOD_MS);
+    modes = (modes + 1) % 2;
   }
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   if (!radio.begin()) {
     Serial.println("Radio hardware is not responding!");
     while (1);
   }
-  radio.openWritingPipe(address[0]);
-  radio.openReadingPipe(1, address[1]);
-  radio.setPALevel(RF24_PA_MAX);
-  Wire.begin(SDA, SCL);
-  xTaskCreatePinnedToCore(nrf_receive_Task, "clock", 2048, NULL, 1, NULL, 1);
+
+  radio.openReadingPipe(1, address);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.setChannel(100);
+  radio.startListening();
+  
+  xTaskCreatePinnedToCore(clock_mode, "clock_mode", 2048, NULL, 1, NULL, 1);
 }
 
-void loop(){
-  if(mode == 0){
+void loop() {
+  if(modes == 0){
     radio.stopListening();
-    radio.write(&data, sizeof(send_data));
+    delay(10);
+    const char text[] = "Hello, drone";
+    radio.write(&text, sizeof(text));
+    Serial.println("message sent!");
   }else{
     radio.startListening();
-    if (radio.available()){
-      char receivedData[64] = {0};
-      radio.read(&receivedData, sizeof(receivedData));
-      char *token = strtok(receivedData, " ");
-      int index = 0;
-      while (token != NULL && index < 4) {
-        data[index] = atoi(token);
-        index++;
-        token = strtok(NULL, " ");
-      }
+    delay(10);
+    char text[32] = "";
+    if(radio.available()){
+      radio.read(&text, sizeof(text));
+      Serial.print("Received: ");
+      Serial.println(text);
+    }else{
+      Serial.println("can't receive the message");
     }
-    for(int i = 0; i < 4; i++){
-      Serial.print(data[i]);
-    }
-    Serial.println();
   }
 }
--- sender.ino
+++ sender.ino
@@ -1,92 +1,50 @@
+#include <SPI.h>
 #include <RF24.h>
-#include <Wire.h>
-#define SDA 21
-#define SCL 22
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-RF24 radio(17, 5);
-const byte address[][6] = {"00001", "00002"};
-int data[4] = {};
-bool mode = 0; // 0 == TX, 1 == RX
-bool connected = false;
-int lost_connected = 0;
-int send_data = 12321;
+RF24 radio(4, 5);
+const byte address[6] = "00001";
+int modes = 1;
+// TaskHandle_t clockModeHandle = NULL;
 
-void clock(void *pvParam){
-  vTaskDelay(200 / portTICK_PERIOD_MS);
-  mode++;
-  if(mode > 2){
-    mode = 0;
-  }
-}
-
-void nrf_receive_Task(void *pvParam){
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  while (1) {
-    if(mode == 1){
-
-    }
-    radio.startListening();
-    if (radio.available()){
-      char receivedData[64] = {0};
-      radio.read(&receivedData, sizeof(receivedData));
-      char *token = strtok(receivedData, " ");
-      int index = 0;
-      while (token != NULL && index < 4) {
-        data[index] = atoi(token);
-        index++;
-        token = strtok(NULL, " ");
-      }
-    }
-    for(int i = 0; i < 4; i++){
-      Serial.print(data[i]);
-    }
-    Serial.println();
+void clock_mode(void *pvParam) {
+  for (;;) {
     vTaskDelay(200 / portTICK_PERIOD_MS);
+    modes = (modes + 1) % 2;
   }
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   if (!radio.begin()) {
     Serial.println("Radio hardware is not responding!");
     while (1);
   }
-  radio.openWritingPipe(address[0]);
-  radio.openReadingPipe(1, address[1]);
-  radio.setPALevel(RF24_PA_MAX);
-  Wire.begin(SDA, SCL);
-  xTaskCreatePinnedToCore(nrf_receive_Task, "clock", 2048, NULL, 1, NULL, 1);
+
+  radio.openReadingPipe(1, address);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.setChannel(100);
+  radio.startListening();
+  
+  xTaskCreatePinnedToCore(clock_mode, "clock_mode", 2048, NULL, 1, NULL, 1);
 }
 
-void loop(){
-  if(connected == false){
-
+void loop() {
+  if(modes == 0){
+    radio.stopListening();
+    delay(10);
+    const char text[] = "Hello, ground";
+    radio.write(&text, sizeof(text));
+    Serial.println("message sent!");
   }else{
-    if(lost_connected >= 3){
-      connected = false;
-    }
-    if(mode == 0){
-      radio.stopListening();
-      radio.write(&data, sizeof(send_data));
-    }else{
-      radio.startListening();
-      if(radio.available()){
-        char receivedData[64] = {0};
-        radio.read(&receivedData, sizeof(receivedData));
-        char *token = strtok(receivedData, " ");
-        int index = 0;
-        while (token != NULL && index < 4) {
-          data[index] = atoi(token);
-          index++;
-          token = strtok(NULL, " ");
-        }
-      }else{
-        lost_connected++;
-      }
-      for(int i = 0; i < 4; i++){
-        Serial.print(data[i]);
-      }
-      Serial.println();
+    radio.startListening();
+    delay(10);
+    char text[32] = "";
+    if(radio.available()){
+      radio.read(&text, sizeof(text));
+      Serial.print("Received: ");
+      Serial.println(text);
     }
   }
 }
